27afe37a6523a1cdef2980f6feda0b2c0f37f33e
Nikolas Laus <bchik3@gmail.com>
2021-05-02 20:42:39 +0300

    Move editing and deleting buttons to bottom.

--- pages/wishes.vue
+++ pages/wishes.vue
@@ -8,27 +8,19 @@
       </template>
       <template v-else-if="wishes && user">
         <v-row v-for="wish in wishes" :key="wish.id" class="mb-3 justify-center">
-          <v-card width="700px" max-width="700px">
-            <v-card-title>{{ wish.name }} <v-spacer/><div class="subtitle-1"><v-avatar size="28" :color="avatarColor(userFullname(wish.user))" class="mr-1">{{ userInitials(wish.user) }}</v-avatar>
-              {{ userFullname(user) }}</div></v-card-title>
+          <v-card width="700px" max-width="700px" elevation="3">
+            <v-card-title>{{ wish.name }}
+              <v-spacer/>
+              <div class="subtitle-1">
+                <v-avatar :color="avatarColor(userFullname(wish.user))" class="mr-1" size="28">
+                  {{ userInitials(wish.user) }}
+                </v-avatar>
+                {{ userFullname(user) }}
+              </div>
+            </v-card-title>
             <v-divider/>
             <v-card-text>
-              <template v-if="wish.user.id === user.id">
-                <v-row>
-                  <v-col cols="10"><p>{{ wish.description }}</p></v-col>
-                  <v-col cols="2">
-                    <v-btn color="error" icon>
-                      <v-icon>mdi-close</v-icon>
-                    </v-btn>
-                    <v-btn color="success" icon>
-                      <v-icon>mdi-pencil</v-icon>
-                    </v-btn>
-                  </v-col>
-                </v-row>
-              </template>
-              <template v-else>
-                <p>{{ wish.description }}</p>
-              </template>
+              <p>{{ wish.description }}</p>
             </v-card-text>
             <v-card-actions>
               <v-row class="align-center">
@@ -41,6 +33,24 @@
                   <v-btn icon>
                     <v-icon>mdi-heart</v-icon>
                   </v-btn>
+                </v-col>
+              </v-row>
+            </v-card-actions>
+            <v-divider/>
+            <v-card-actions>
+              <v-row class="align-center">
+                <v-col cols="6" class="text-caption">
+                  <span class="ml-2">This wish is created by <strong>you.</strong></span>
+                </v-col>
+                <v-col cols="6" class="text-right">
+                  <template v-if="wish.user.id === user.id" class="mr-2">
+                    <v-btn color="error" icon>
+                      <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <v-btn color="success" icon>
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+                  </template>
                 </v-col>
               </v-row>
             </v-card-actions>
