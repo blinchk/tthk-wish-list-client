a6305eed1100e1bbffa67966881a33ec4f379112
Nikolas Laus <bchik3@gmail.com>
2021-06-10 12:19:56 +0300

    Add gift dialog to watch selected gift.

--- pages/profile/_id.vue
+++ pages/profile/_id.vue
@@ -1,6 +1,11 @@
 <template>
   <v-layout wrap>
     <v-col cols="12">
+      <template v-if="loading">
+        <v-row class="justify-center">
+          <v-progress-circular color="primary" indeterminate></v-progress-circular>
+        </v-row>
+      </template>
       <v-row class="mb-3 justify-center">
         <v-card v-if="!loading" :loading="loading" elevation="3" max-width="700px" width="700px">
           <v-card-title>User info</v-card-title>
@@ -12,12 +17,14 @@
           </v-list>
           <v-col :hidden="profile.user.id === this.user.id" class="text-right">
             <v-btn v-if="!profile.following" :loading="followLoading" color="success"
-                   @click.stop="followUser(profile.user.id)">
+                   @click.stop="followUser(profile.user.id)"
+            >
               <v-icon class="mr-1">mdi-account-plus-outline</v-icon>
               Follow
             </v-btn>
             <v-btn v-else-if="profile.following" :loading="followLoading" color="error"
-                   @click.stop="followUser(profile.user.id)">
+                   @click.stop="followUser(profile.user.id)"
+            >
               <v-icon class="mr-1">mdi-account-minus-outline</v-icon>
               Unfollow
             </v-btn>
@@ -25,15 +32,11 @@
 
         </v-card>
       </v-row>
-      <template v-if="loading">
-        <v-row class="justify-center">
-          <v-progress-circular color="primary" indeterminate></v-progress-circular>
-        </v-row>
-      </template>
       <template v-if="!loading">
         <v-row v-for="wish in profile.wishes"
                :key="wish.id"
-               class="mb-3 justify-center">
+               class="mb-3 justify-center"
+        >
           <v-card elevation="3" max-width="700px" width="700px">
             <v-card-title>{{ wish.name }}
               <v-spacer/>
@@ -80,17 +83,42 @@
 
                 </v-col>
               </v-row>
-              <v-tooltip bottom v-if="wish.gifted">
+              <v-tooltip v-if="wish.gifted" bottom>
                 <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon v-bind="attrs" v-on="on" v-if="profile.user.id !== user.id" @click.stop="bookGift(wish)">
+                  <v-btn v-if="profile.user.id !== user.id" v-bind="attrs" v-on="on" icon @click.stop="showGift(wish)">
                     <v-icon>mdi-gift</v-icon>
                   </v-btn>
                 </template>
-                <span>Book the gift.</span>
+                <span>Show the gift.</span>
               </v-tooltip>
             </v-card-actions>
             <v-divider/>
           </v-card>
+          <v-dialog v-model="giftDialogOpen" max-width="500px">
+            <v-card v-if="currentGift">
+              <v-card-title>{{ currentGift.title }}</v-card-title>
+              <v-card-text><a :href="currentGift.link">Link to gift</a></v-card-text>
+              <v-card-actions class="justify-end">
+                <v-btn v-if="!currentGift.user_booked.id" :loading="bookingIsLoading" color="success" text
+                       @click.stop="bookGift(currentGift.wish)"
+                >
+                  <v-icon left>mdi-gift</v-icon>
+                  Book Gift
+                </v-btn>
+                <v-btn v-else-if="currentGift.user_booked.id === user.id" :loading="bookingIsLoading" color="error" text
+                       @click.stop="bookGift(currentGift.wish)"
+                >
+                  <v-icon left>mdi-gift</v-icon>
+                  Unbook Gift
+                </v-btn>
+                <v-btn v-else :loading="bookingIsLoading" color="error" disabled
+                       text
+                >
+                  Gift is booked
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-row>
       </template>
     </v-col>
@@ -98,30 +126,34 @@
 </template>
 
 <script>
-import {mapActions, mapMutations, mapState} from 'vuex'
+import { mapActions, mapMutations, mapState } from 'vuex'
 import moment from 'moment'
 import randomMC from 'random-material-color'
 
 export default {
-  name: "user",
-  data() {
+  name: 'user',
+  data () {
     return {
       loading: false,
       showPeople: false,
+      giftDialogOpen: false,
       followLoading: false,
+      giftIsLoading: false,
+      currentGift: null,
+      bookingIsLoading: false,
       moment
     }
   },
-  created() {
+  created () {
     this.loading = true
   },
-  mounted() {
-    if (!this.accessToken) {
-      this.loading = this.checkForToken()
-    } else if (this.user) {
+  mounted () {
+    this.checkForToken().then(() => {
       this.getUserProfile(this.$route.params.id)
-      this.getGifts(this.$route.params.id)
-    }
+      if (this.user) {
+        this.getGifts(this.$route.params.id)
+      }
+    })
   },
   computed: {
     ...mapState('users', ['profile']),
@@ -130,9 +162,9 @@ export default {
   }
   ,
   methods: {
-    ...mapActions(['getUser, checkForToken']),
+    ...mapActions(['getUser', 'checkForToken']),
     ...mapMutations(['createNewAlert']),
-    getUserProfile(user) {
+    getUserProfile (user) {
       this.$store.dispatch('users/getUserProfile', {
         id: user
       }).then(() => {
@@ -140,19 +172,19 @@ export default {
       })
     }
     ,
-    userFullname(user) {
+    userFullname (user) {
       return user.firstName + ' ' + user.lastName
     }
     ,
-    userInitials(user) {
+    userInitials (user) {
       return user.firstName[0] + user.lastName[0]
     }
     ,
-    avatarColor(fullName) {
-      return randomMC.getColor({text: fullName})
+    avatarColor (fullName) {
+      return randomMC.getColor({ text: fullName })
     }
     ,
-    toggleLike(wish) {
+    toggleLike (wish) {
       this.$store.dispatch('wishes/addLike', {
         connection: wish.id
       }).then(() => {
@@ -160,14 +192,14 @@ export default {
       })
     }
     ,
-    showLiked(wish) {
+    showLiked (wish) {
       this.showPeople = true
       this.$store.dispatch('wishes/peopleLiked', {
         wish: wish
       })
     }
     ,
-    followUser(user) {
+    followUser (user) {
       this.$store.dispatch('users/followUser', {
         id: user
       }).then(() => {
@@ -175,17 +207,30 @@ export default {
         this.followLoading = false
       })
     },
-    getGifts(user) {
+    getGifts (user) {
       this.$store.dispatch('wishes/getGifts', {
         user: user
       })
     },
-    bookGift(wish){
-      this.$store.dispatch('wishes/bookGift',{
+    showGift (wish) {
+      this.giftDialogOpen = true
+      this.loadGift(wish)
+    },
+    loadGift (wish) {
+      this.$store.dispatch('wishes/getGiftByWish', {
         wish: wish
-      }).then(()=>{
-        this.getUserProfile(this.$route.params.id)
-        this.getGifts(this.$route.params.id)
+      }).then((gift) => {
+        this.giftIsLoading = false
+        this.currentGift = gift
+      })
+    },
+    bookGift (wish) {
+      this.bookingIsLoading = true
+      this.$store.dispatch('wishes/bookGift', {
+        wish: wish
+      }).then(() => {
+        this.bookingIsLoading = false
+        this.loadGift(wish)
       })
     }
   }
